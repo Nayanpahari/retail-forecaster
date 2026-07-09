d689fdb7e06462b2e5e679c18aad286a57bbe58d
Nayanpahari <nayanpahari434@gmail.com>
2026-07-09 20:49:25 +0530

    Fix: Use SQLite fallback for deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import DATABASE_URL
+#for render only
+import os
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)# for render only
+
+#engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
